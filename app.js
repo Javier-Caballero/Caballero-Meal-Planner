56c9a45526b3136a4431bfcbbbfc0ae2ee0236e5
Javier_Caballero <javier.caballero@mac.com>
2021-03-27 13:48:33 -0400

    added db and model

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require('express');
-const meals = require('./routes/meals');
+const db = require('./db');
+const router = require('./routes');
 const app = express();
 
-app.use('/', meals);
+const { Meal } = db.models;
+
+//app.use(express.json());
+
+app.use('/', router);
+
+(async () => {
+    await db.sequelize.sync({ force: true });
+
+    try {
+    const meal = await Meal.create({
+        title: 'Mexi Bowl'
+    });
+    console.log(meal.toJSON());
+
+    const meal2 = await Meal.create({
+        title: 'Broccoli Bowl'
+    });
+    console.log(meal2.toJSON());
+
+    } catch (error) {
+    console.error('Error connecting to the database: ', error);
+    }
+})();
 
 app.listen(3000, () => {
     console.log('Caballero Meal Planner listening on port 3000');
-});   
+});
--- db/models/meal.js
+++ db/models/meal.js
@@ -1,31 +1,22 @@
-'use strict';
 const Sequelize = require('sequelize');
 
 module.exports = (sequelize) => {
-  class Meal extends Sequelize.Model {
-      publishedAt() {
-          const date = moment(this.createdAt).format('MMMM D, YYYY, h:mma');
-          return date;
-      }
-      shortDescription() {
-          const shortDesc = this.body.length > 200 ? this.body.substring(0, 200) + '...' : this.body;
-          return shortDesc;
-      }
-  }
+  class Meal extends Sequelize.Model {}
   Meal.init({
-    title: {
-        type: Sequelize.STRING,
-        validate: {
-          notEmpty: {
-            msg: '"Title" is required'
-          }
+  title: {
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: {
+          msg: '"Title" is required'
         }
-      },
-    
-    time: Sequelize.STRING,
-    ingredients: Sequelize.TEXT,
-    instructions: Sequelize.TEXT
-  }, { sequelize });
+      }
+    },
+  
+  time: Sequelize.STRING,
+  ingredients: Sequelize.TEXT,
+  instructions: Sequelize.TEXT
+}, { sequelize });
+
 
   return Meal;
 };
